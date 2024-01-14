20e0fdc34062263c86a84d94c659ec55037e6ea1
Bartosz7 <bartoszgrabek7@gmail.com>
2024-01-14 16:47:22 +0100

    feat: added function to add skos:narrower to concept files

--- convert_to_jsonld.py
+++ convert_to_jsonld.py
@@ -1,6 +1,8 @@
-# This file includes functions used to convert
-# nodes into jsonld files
-# usage: convert_to_jsonld.py [-h] input_file [destination]
+# This file prepares concept JSONLD files and an index_dict.json file
+# for the OpenCS web browser:
+# - adds a skos:narrower property to each concept for navigation
+# - converts and saves each concept in a JSONLD file
+# usage: prepare_browser_files.py [-h] input_file [destination]
 
 import os
 import argparse
@@ -58,7 +60,7 @@ def get_dirname_from_concept(filename):
     Follows the same scheme for core structure of .ttl files, namely
     a directory per 1000 files"""
     concept_number = int(filename.split("C")[1])
-    directory = f"{concept_number // 1000:02}"  # 1000 files per dir
+    directory = f"{concept_number // 1000:02}"
     return directory
 
 
@@ -72,6 +74,15 @@ def load_graph(file, format="turtle"):
 def select_triples_with_query(graph: Graph, query: str):
     triples = graph.query(query)
     return triples
+
+
+def add_skos_narrower_property(graph: Graph):
+    new_graph = Graph()
+    for (s, p, o) in graph:
+        new_graph.add((s, p, o))
+        if p == SKOS.broader:
+            new_graph.add((o, SKOS.narrower, s))
+    return new_graph
 
 
 def save_graph_to_jsonld(graph: Graph, filepath: str):
@@ -109,6 +120,8 @@ def main():
     concept_dict = {}  # dictionary to store (conceptId, prefLabel) for browser
     args = parse_arguments()
     openCS = load_graph(args.input)
+    # preprocess graph
+    openCS = add_skos_narrower_property(openCS)
     # serialise individual concepts as jsonld files
     with change_directory(args.destination):
         concept_triples = select_triples_with_query(openCS,
