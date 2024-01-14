7f76487e03a8b136ce8275700c2637107dec66db
Bartosz7 <bartoszgrabek7@gmail.com>
2024-01-14 19:09:01 +0100

    fix: correct path for index_dict

--- prepare_browser_data.py
+++ prepare_browser_data.py
@@ -116,6 +116,7 @@ def process_concepts(graph, concepts, concept_dict):
         with change_directory(get_dirname_from_concept(str(concept_uri))):
             process_single_concept(graph, concept_uri, concept_dict)
 
+
 def main():
     concept_dict = {}  # dictionary to store (conceptId, prefLabel) for browser
     args = parse_arguments()
@@ -130,14 +131,14 @@ def main():
                                                     SELECT_CONCEPTS_QUERY)
         process_concepts(openCS, concept_triples, concept_dict)
         print("Finished processing concept files")
-    # save dictionary with (prefLabel, conceptId)
-    print(os.getcwd())
-    print(os.listdir())
-    filepath = os.path.join(os.getcwd(), "core", "index_dict.json")
-    print(filepath)
-    with open(filepath, "w") as file:
-        json.dump(concept_dict, file, indent=4)
-    print("Index dictionary saved")
+        # save dictionary with (prefLabel, conceptId)
+        print(os.getcwd())
+        os.makedirs(os.path.join(os.getcwd(), "core"), exist_ok=True)
+        filepath = os.path.join(os.getcwd(), "core", "index_dict.json")
+        print(filepath)
+        with open(filepath, "w") as file:
+            json.dump(concept_dict, file, indent=4)
+        print("Index dictionary saved")
 
 
 if __name__ == "__main__":
